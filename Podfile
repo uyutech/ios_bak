00f6c3d5e9e7a15e736339bbc427a9ae1867f31a
ydream <ydream17@gmail.com>
2017-04-25 23:54:39 +0800

    init ========

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+target 'ZhuanquanIOS' do
+	pod 'Masonry'
+	pod 'AFNetworking', '~> 2.5.3'
+	pod 'SDWebImage', '~> 3.7.2'
+end
---
+++ ZhuanquanIOS/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  ZhuanquanIOS
+//
+//  Created by ydream on 2017/4/25.
+//  Copyright © 2017年 ydream. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *view = [[UIView alloc] init];
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+    }];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
