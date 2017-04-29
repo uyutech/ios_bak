42917dc5531ca4ed9c972824b1f853e428a34f90
ydream <ydream17@gmail.com>
2017-04-29 22:30:39 +0800

    loginPanel ======

--- ZhuanquanIOS/LoginView.h
+++ ZhuanquanIOS/LoginView.h
@@ -8,9 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+#define COLOR_DARK_BLUE [UIColor colorWithHexString:@"#4c8daf"]
+#define COLOR_LIGHT_BLUE [UIColor colorWithHexString:@"#b4cfdd"]
+#define COLOR_INPUT_BG [UIColor colorWithHexString:@"#edf3f7"]
+
+
 @interface LoginView : UIView
 
 @property (nonatomic, weak) UIButton *loginTabButton;
 @property (nonatomic, weak) UIButton *regTabButton;
+@property (nonatomic, weak) UIButton *forgetButton;
+
+@property (nonatomic, weak) UIButton *loginSubmitButton;
+@property (nonatomic, weak) UIButton *resetSubmitButton;
+@property (nonatomic, weak) UIButton *regSubmitButton;
+
+@property (nonatomic, weak) UITextField *passport;
+@property (nonatomic, weak) UITextField *password;
 
 @end
--- ZhuanquanIOS/LoginView.m
+++ ZhuanquanIOS/LoginView.m
@@ -18,6 +18,10 @@
 @property (nonatomic, weak) UIView *active;
 @property (nonatomic, weak) UIButton *selectedButton;
 
+@property (nonatomic, weak) UIView *loginPanel;
+@property (nonatomic, weak) UIView *resetPanel;
+@property (nonatomic, weak) UIView *regPanel;
+
 @end
 
 @implementation LoginView
@@ -27,6 +31,7 @@
     if (self = [super initWithFrame:frame]) {
         [self buildLoginFrame];
         [self buildLoginTab];
+        [self buildLoginPanel];
     }
     
     return self;
@@ -65,25 +70,11 @@
 
 - (void)buildLoginTab {
     
-    UIColor *colorDarkBlue = [UIColor colorWithHexString:@"#4c8daf"];
-    UIColor *colorLightBlue = [UIColor colorWithHexString:@"#d8e4eb"];
-    
-    UIButton *loginTabButton = [[UIButton alloc] init];
-    [loginTabButton setTitle:@"登录" forState: UIControlStateNormal];
-    [loginTabButton setTitleColor:colorDarkBlue forState:UIControlStateSelected];
-    [loginTabButton setTitleColor:colorLightBlue forState:UIControlStateNormal];
-    loginTabButton.titleLabel.font = [UIFont systemFontOfSize:16];
-    loginTabButton.titleLabel.textAlignment = NSTextAlignmentCenter;
-    
-    UIButton *regTabButton = [[UIButton alloc] init];
-    [regTabButton setTitle:@"注册" forState: UIControlStateNormal];
-    [regTabButton setTitleColor:colorDarkBlue forState:UIControlStateSelected];
-    [regTabButton setTitleColor:colorLightBlue forState:UIControlStateNormal];
-    regTabButton.titleLabel.font = [UIFont systemFontOfSize:16];
-    regTabButton.titleLabel.textAlignment = NSTextAlignmentCenter;
-    
+    UIButton *loginTabButton = [self createTabButton:@"登录"];
+    UIButton *regTabButton = [self createTabButton:@"注册"];
     UIView *active = [[UIView alloc] init];
-    active.backgroundColor = colorDarkBlue;
+    
+    active.backgroundColor = COLOR_DARK_BLUE;
     
     _loginTabButton = loginTabButton;
     _regTabButton = regTabButton;
@@ -115,11 +106,120 @@
     [regTabButton addTarget:self action:@selector(setSelected:) forControlEvents:UIControlEventTouchUpInside];
 }
 
+- (void)buildLoginPanel {
+    
+    UIView *loginPanel = [[UIView alloc] init];
+    
+    UITextField *passport = [self createTextField:@"请输入手机号"];
+    UITextField *password = [self createTextField:@"请输入密码"];
+    
+    password.secureTextEntry = YES;
+    
+    UIButton *forgetButton = [[UIButton alloc] init];
+    [forgetButton setTitle:@"忘记密码" forState:UIControlStateNormal];
+    [forgetButton setTitleColor:COLOR_DARK_BLUE forState:UIControlStateNormal];
+    forgetButton.titleLabel.font = [UIFont systemFontOfSize:13];
+    forgetButton.titleLabel.textAlignment = NSTextAlignmentRight;
+    
+    UIButton *loginSubmitButton = [self createSubmitButton:@"登 录"];
+    
+    _passport = passport;
+    _password = password;
+    _forgetButton = forgetButton;
+    _loginSubmitButton = loginSubmitButton;
+    _loginPanel = loginPanel;
+    
+    [loginPanel addSubview:passport];
+    [loginPanel addSubview:password];
+    [loginPanel addSubview:forgetButton];
+    [loginPanel addSubview:loginSubmitButton];
+    [self addSubview:loginPanel];
+    
+    [loginPanel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_loginFrame).with.offset(52);
+        make.left.equalTo(_loginFrame).with.offset(0);
+        make.right.equalTo(_loginFrame).with.offset(0);
+        make.height.mas_greaterThanOrEqualTo(_loginFrame.mas_height);
+    }];
+    
+    [passport mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(loginPanel).with.offset(0);
+        make.left.equalTo(loginPanel).with.offset(18);
+        make.right.equalTo(loginPanel).with.offset(-18);
+        make.height.mas_equalTo(40);
+    }];
+    
+    [password mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(passport.mas_bottom).with.offset(10);
+        make.left.equalTo(loginPanel).with.offset(18);
+        make.right.equalTo(loginPanel).with.offset(-18);
+        make.height.mas_equalTo(40);
+    }];
+    
+    [forgetButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(password.mas_bottom).with.offset(10);
+        make.right.equalTo(loginPanel).with.offset(-18);
+        make.width.mas_greaterThanOrEqualTo(60);
+        make.height.mas_equalTo(15);
+    }];
+    
+    [loginSubmitButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(password.mas_bottom).with.offset(36);
+        make.left.equalTo(loginPanel).with.offset(18);
+        make.right.equalTo(loginPanel).with.offset(-18);
+        make.height.mas_equalTo(40);
+    }];
+}
+
+- (void)buildRegPanel {
+    
+}
+
+- (void)buildResetPasswordPanel {
+    
+}
+
+- (UITextField *)createTextField:(NSString *)withPlaceholder {
+    
+    UITextField *textField = [[UITextField alloc] init];
+    textField.backgroundColor = COLOR_INPUT_BG;
+    textField.layer.cornerRadius = 5.0f;
+    [textField setPlaceholder:withPlaceholder ?: @""];
+    [textField setTextColor:COLOR_DARK_BLUE];
+    [textField setValue:COLOR_LIGHT_BLUE forKeyPath:@"_placeholderLabel.textColor"];
+    [textField setFont:[UIFont systemFontOfSize:14]];
+    [textField setClearButtonMode:UITextFieldViewModeWhileEditing];
+    
+    return textField;
+}
+
+- (UIButton *)createTabButton:(NSString *)title {
+    
+    UIButton *tabButton = [[UIButton alloc] init];
+    [tabButton setTitle:title forState: UIControlStateNormal];
+    [tabButton setTitleColor:COLOR_DARK_BLUE forState:UIControlStateSelected];
+    [tabButton setTitleColor:COLOR_LIGHT_BLUE forState:UIControlStateNormal];
+    tabButton.titleLabel.font = [UIFont systemFontOfSize:16];
+    tabButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    return tabButton;
+}
+
+- (UIButton *)createSubmitButton:(NSString *)title {
+    
+    UIButton *button = [[UIButton alloc] init];
+    button.backgroundColor = COLOR_DARK_BLUE;
+    button.layer.cornerRadius = 20.0f;
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+
+    return button;
+}
+
+/// User Actions
+
 - (void)setSelected:(UIButton *)target {
     
-    [target setSelected:YES];
-    [_selectedButton setSelected:NO];
-
     [_active mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(target.mas_bottom).with.offset(-8);
         make.centerX.mas_equalTo(target.mas_centerX);
@@ -138,6 +238,7 @@
     __weak __typeof(&*self) weakSelf = self;
     
     if (_selectedButton != nil) {
+        [_selectedButton setSelected:NO];
         [UIView animateWithDuration:0.25 animations:^{
             [weakSelf layoutIfNeeded];
         } completion:^(BOOL finished) {
@@ -145,6 +246,7 @@
         }];
     }
     
+    [target setSelected:YES];
     _selectedButton = target;
 }
 
